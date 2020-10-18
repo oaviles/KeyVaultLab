9ad85187bb55bc872f7f458eab20c6c34eebf80e
Omar Aviles Rosas <edaviles@microsoft.com>
2020-10-18 14:12:56 +0000

    Updated Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace KeyVaultDemoApp2
+namespace KeyVaultDemoApp
 {
     public class Startup
     {
